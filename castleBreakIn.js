3556141e90597b54cc8e5479c009f763950b8b79
Rob Fink <finkr@dewv.edu>
2018-09-19 16:56:38 +0000

    Updates via sync script

--- castleBreakIn.js
+++ castleBreakIn.js
@@ -13,10 +13,13 @@ class Wall extends Sprite {
     }
 }
 
+//Creates walls
 new Wall(0, 0, "A spooky castle wall", "castle.png");
 let leftWall = new Wall(0, 200, "Left side Wall", "wall.png");
 let rightWall = new Wall(game.displayWidth - 48, 200, "Right side Wall", "wall.png");
 
+
+//Creates Princess Class
 class Princess extends Sprite {
     constructor() {
         super();
@@ -24,15 +27,53 @@ class Princess extends Sprite {
         this.setImage("ann.png");
         this.height = 48;
         this.width = 48;
-        this.x = game.displayWidth;
+        this.x = game.displayWidth / 2;
         this.y = game.displayHeight - this.height;
         this.speedWhenWalking = 150;
         this.lives = 1;
         this.accelerateOnBounce = false;
+        this.defineAnimation ("left", 9, 11);
+        this.defineAnimation("right", 3, 5);
+        }
+    
+            // Move Left  
+            handleLeftArrowKey() {
+            this.playAnimation("left");
+            this.speed = this.speedWhenWalking;
+            this.angle = 180;
+            }
+
+            // Move Right
+            handleRightArrowKey() {
+            this.playAnimation("right");
+            this.speed = this.speedWhenWalking;
+            this.angle = 0;
+            }
         
-
-    }
-
-
-
+            // Keeps the Princess in the Playing Area
+            handleGameLoop() {
+            this.speed = 0;
+            this.x = Math.min(game.displayWidth - rightWall.width - this.width, this.x);
+            this.x = Math.max(leftWall.width, this.x);
+            }
 }
+
+let ann = new Princess;
+
+//Creates Ball
+class Ball extends Sprite{
+    constructor (){
+        super();
+        this.height = 48;
+        this.width = 48;
+        this.x = this.width / 2;
+        this.y = this.height / 2;
+        this.name = "Ball";
+        this.setImage("ball.png");
+        this.defineAnimation("spin", 0, 12);
+        this.speed = 1;
+        this.angle = 50 + Math.random() * 80;
+    }
+}
+
+let ball = new Ball;
