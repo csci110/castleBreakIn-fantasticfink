4d9a6d68deded14c31807e3e7ca5a913dfca7fef
Rob Fink <finkr@dewv.edu>
2018-09-24 17:02:10 +0000

    Updates via sync script

--- castleBreakIn.js
+++ castleBreakIn.js
@@ -21,6 +21,7 @@ let rightWall = new Wall(game.displayWidth - 48, 200, "Right side Wall", "wall.p
 
 //Creates Princess Class
 class Princess extends Sprite {
+    
     constructor() {
         super();
         this.name = "Princess Ann";
@@ -32,40 +33,79 @@ class Princess extends Sprite {
         this.speedWhenWalking = 150;
         this.lives = 1;
         this.accelerateOnBounce = false;
-        this.defineAnimation ("left", 9, 11);
+        this.defineAnimation("left", 9, 11);
         this.defineAnimation("right", 3, 5);
-        }
-    
-            // Move Left  
-            handleLeftArrowKey() {
-            this.playAnimation("left");
-            this.speed = this.speedWhenWalking;
-            this.angle = 180;
-            }
+        this.lives = 3;
+    }
 
-            // Move Right
-            handleRightArrowKey() {
-            this.playAnimation("right");
-            this.speed = this.speedWhenWalking;
-            this.angle = 0;
-            }
-        
-            // Keeps the Princess in the Playing Area
-            handleGameLoop() {
-            this.speed = 0;
-            this.x = Math.min(game.displayWidth - rightWall.width - this.width, this.x);
-            this.x = Math.max(leftWall.width, this.x);
-            }
+    // Move Left  
+    handleLeftArrowKey() {
+        this.playAnimation("left");
+        this.speed = this.speedWhenWalking;
+        this.angle = 180;
+    }
+
+    // Move Right
+    handleRightArrowKey() {
+        this.playAnimation("right");
+        this.speed = this.speedWhenWalking;
+        this.angle = 0;
+    }
+
+    // Keeps the Princess in the Playing Area
+    handleGameLoop() {
+        this.speed = 0;
+        this.x = Math.min(game.displayWidth - rightWall.width - this.width, this.x);
+        this.x = Math.max(leftWall.width, this.x);
+    }
+
+    handleCollision(otherSprite) {
+        // Horizontally, Ann's image file is about one-third blank, one-third Ann, and one-third blank.
+        // Vertically, there is very little blank space. Ann's head is about one-fourth the height.
+        // The other sprite (Ball) should change angle if:
+        // 1. it hits the middle horizontal third of the image, which is not blank, AND
+        // 2. it hits the upper fourth, which is Ann's head.
+        let horizontalOffset = this.x - otherSprite.x;
+        let verticalOffset = this.y - otherSprite.y;
+        if (Math.abs(horizontalOffset) < this.width / 3 &&
+            verticalOffset > this.height / 4) {
+            // The new angle depends on the horizontal difference between sprites.
+            otherSprite.angle = 90 + 2 * horizontalOffset;
+        }
+        return false;
+    }
+
+    handleFirstGameLoop() {
+        // Set up a text area to display the number of lives remaining.
+        this.livesDisplay = game.createTextArea(game.displayWidth - 144, 20);
+        this.updateLivesDisplay();
+    }
+
+    updateLivesDisplay() {
+        game.writeToTextArea(this.livesDisplay, "Lives = " + this.lives);
+    }
+
+    loseALife() {
+        this.lives = this.lives - 1;
+        this.updateLivesDisplay();
+        if (this.lives > 0) {
+            new Ball;
+        }
+        else {
+            game.end('The mysterious stranger \nhas escaped Princess Ann\nfor now!  Better luck next time.');
+        }
+    }
 }
 
 let ann = new Princess;
 
 //Creates Ball
-class Ball extends Sprite{
-    constructor (){
+class Ball extends Sprite {
+    
+    constructor() {
         super();
-        this.x = game.displayWidth/2;
-        this.y = game.displayHeight/2;
+        this.x = game.displayWidth / 2;
+        this.y = game.displayHeight / 2;
         this.height = 48;
         this.width = 48;
         this.name = "Ball";
@@ -75,10 +115,15 @@ class Ball extends Sprite{
         this.speed = 1;
         this.angle = 50 + Math.random() * 80;
     }
-    
-    handleGameLoop(){
-        if (this.speed < 200)
+
+    handleGameLoop() {
+        if (this.speed <= 200)
             this.speed += 2;
+    }
+
+    handleBoundaryContact() {
+        game.removeSprite(this);
+        ann.loseALife();
     }
 }
 
